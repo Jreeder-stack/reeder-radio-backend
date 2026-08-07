52891f6e89043c8ee4f912319e2e48cfe34dd6e9
Jreeder-stack <227722155+Jreeder-stack@users.noreply.github.com>
2026-08-06 20:00:56 -0400

    feat: install V3 dispatcher runtime at startup

--- src/services/strictCenterIsolationBootstrap.js
+++ src/services/strictCenterIsolationBootstrap.js
@@ -1,3 +1,5 @@
 import { installStrictCenterIsolation } from './strictCenterIsolation.js';
+import { installAiDispatcherV3Runtime } from './aiDispatcherV3RuntimeInstaller.js';
 
 installStrictCenterIsolation();
+installAiDispatcherV3Runtime();
