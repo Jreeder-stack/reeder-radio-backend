ad67a31fca1426a058cd686b6b59df75b8739bd5
Jreeder-stack <227722155+Jreeder-stack@users.noreply.github.com>
2026-08-06 20:42:16 -0400

    cleanup: remove legacy AI bootstrap

--- src/services/strictCenterIsolationBootstrap.js
+++
@@ -1,5 +0,0 @@
-import { installStrictCenterIsolation } from './strictCenterIsolation.js';
-import { installAiDispatcherV3Runtime } from './aiDispatcherV3RuntimeInstaller.js';
-
-installStrictCenterIsolation();
-installAiDispatcherV3Runtime();
